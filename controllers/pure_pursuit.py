update traj, correct lookahead point

controllers/pure_pursuit.py
@@ -87,15 +87,15 @@ class PurePursuit:
 
 def get_lookahead_point(offset_traj):
     num_points = offset_traj.shape[0]
-    steps = np.linspace(start=1, stop=num_points, num=num_points, endpoint=False)
+    steps = np.linspace(start=0, stop=num_points, num=num_points, endpoint=False)  # 0 ~ 9
 
-    point = np.zeros(2)  #
-    for i in range(2):  # offset_traj.shape[1] = 2, [x, y]
+    profile = np.zeros(3)
+    for i in range(3):  # offset_traj.shape[1] = 3, [x, y, v]
         val = offset_traj[:, i]
         interp_func = interp1d(steps, val.flatten(), kind='cubic')
-        new_steps = np.linspace(start=1, stop=num_points, num=4, endpoint=False)  # 0.5s predict time to chase
+        new_steps = np.linspace(start=0, stop=num_points, num=4, endpoint=False)  # 0.5s predict time to chase
         new_val = interp_func(new_steps)
-        point[i] = new_val[0]  # the first point
+        profile[i] = new_val[1]  # the second point
 
-    return point
+    return profile
 

utils/render.py
@@ -2,6 +2,7 @@ import numpy as np
 from pyglet.gl import *  # game interface
 # from pyglet.gl import GL_POINTS, GL_QUADS, GL_TRIANGLES  # game interface
 
+
 class Renderer:
 
     def __init__(self, waypoints):
@@ -10,13 +11,12 @@ class Renderer:
 
         self.front_traj = None
         self.last_front_traj = []
-        
+
         self.horizon_traj = None
         self.last_horizon_traj = []
-        
+
         self.ahead_point = None
         self.last_ahead_point = None
-        
 
     def render_waypoints(self, e):
         """
@@ -43,7 +43,7 @@ class Renderer:
             point = self.front_traj[:, 1:3]
         else:
             point = self.front_traj[:, :2]
-        
+
         # point = np.vstack((x, y)).T
         scaled_point = 50. * point
 
@@ -55,16 +55,17 @@ class Renderer:
             b = e.batch.add(1, GL_POINTS, None, ('v3f/stream', [scaled_point[i, 0], scaled_point[i, 1], 0.]),
                             ('c3B/stream', [255, 0, 0]))
             self.last_front_traj.append(b)
-            
+
     def render_horizon_traj(self, e):
         """
         update horizon point
         """
-        def create_triangle(x, y, size):            
-            return ('v2f/stream', [x+size, y,
-                            x-size/2, y-size,
-                            x-size/2, y+size])
-                
+
+        def create_triangle(x, y, size):
+            return ('v2f/stream', [x + size, y,
+                                   x - size / 2, y - size,
+                                   x - size / 2, y + size])
+
         point = self.horizon_traj[:, :2]
         scaled_point = 50. * point
 
@@ -73,10 +74,9 @@ class Renderer:
         self.last_horizon_traj.clear()
 
         for i in range(scaled_point.shape[0]):
-            b = e.batch.add(3, GL_TRIANGLES, None, create_triangle(scaled_point[i,0], scaled_point[i,1], 4),
+            b = e.batch.add(3, GL_TRIANGLES, None, create_triangle(scaled_point[i, 0], scaled_point[i, 1], 4),
                             ('c3B/stream', [255, 255, 0, 255, 255, 0, 255, 255, 0]))
             self.last_horizon_traj.append(b)
-        
 
     def render_lookahead_point(self, e):
         """
@@ -84,19 +84,19 @@ class Renderer:
         """
 
         def create_triangle(x, y, size):
-            return ('v2f/stream', [x, y+size,
-                            x-size, y-size/2,
-                            x+size, y-size/2])
+            return ('v2f/stream', [x, y + size,
+                                   x - size, y - size / 2,
+                                   x + size, y - size / 2])
 
         scaled_point = 50. * self.ahead_point
-        
-        if self.last_ahead_point != None:
+
+        if self.last_ahead_point is not None:
             self.last_ahead_point.delete()
 
         b = e.batch.add(3, GL_TRIANGLES, None, create_triangle(scaled_point[0], scaled_point[1], 10),
-                            ('c3B/stream', [0, 255, 0, 0, 255, 0, 0, 255, 0])) 
+                        ('c3B/stream', [0, 255, 0, 0, 255, 0, 0, 255, 0]))
         self.last_ahead_point = b
-            
+
 
 def fix_gui(e):
     # update camera to follow car

utils/rl_utils.py
@@ -50,13 +50,14 @@ def get_front_traj(obs, profile, predict_time=2):
 
 def get_interpolated_traj_with_horizon(traj, h):
     num_points = traj.shape[0]
-    steps = np.linspace(start=1, stop=num_points, num=num_points, endpoint=False)
+    # linspace - https://numpy.org/doc/stable/reference/generated/numpy.linspace.html
+    steps = np.linspace(start=0, stop=num_points, num=num_points, endpoint=False)
 
     h_traj = []
     for i in range(1, traj.shape[1]):  # x, y, v
         val = traj[:, i]
         interp_func = interp1d(steps, val.flatten(), kind='cubic')
-        new_steps = np.linspace(start=1, stop=num_points, num=h, endpoint=False)
+        new_steps = np.linspace(start=0, stop=num_points, num=h, endpoint=False)
         new_val = interp_func(new_steps)
         h_traj.append(new_val)
     h_traj = np.array(h_traj).T