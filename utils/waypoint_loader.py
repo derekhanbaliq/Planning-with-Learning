050b01b57a276834139e8203f0f6efb0ac0bf769
Derek the Gurkhan <97198239+derekhanbaliq@users.noreply.github.com>
2024-04-18 18:52:10 -0400

    update waypoint loader bugs

--- utils/waypoint_loader.py
+++ utils/waypoint_loader.py
@@ -5,14 +5,13 @@ class WaypointLoader:
     def __init__(self, map_name, csv_data=None):
         self.unit_dist = 0.2
 
-        if (map_name == 'Spielberg' or map_name == 'MoscowRaceway' or map_name == 'Catalunya'
-                or map_name == 'levine_2nd' or map_name == 'skir'):
+        if map_name == 'Spielberg' or map_name == 'MoscowRaceway' or map_name == 'Catalunya':
             self.x = csv_data[:, 1]
             self.y = csv_data[:, 2]
             self.v = csv_data[:, 5]
             self.θ = csv_data[:, 3]  # coordinate matters!
             self.γ = csv_data[:, 4]
-        elif map_name == 'example' or map_name == 'icra' or map_name == 'levine':
+        elif map_name == 'example' or map_name == 'icra' or map_name == 'levine' or map_name == 'skir' or map_name == 'levine_2nd':
             self.x = csv_data[:, 1]
             self.y = csv_data[:, 2]
             self.v = csv_data[:, 5]
