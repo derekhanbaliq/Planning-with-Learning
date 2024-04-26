fixed speed bugs & lookahead point

controllers/pure_pursuit.py
@@ -99,7 +99,7 @@ class PurePursuit:
         return targetPoint, target_point_index
 
 
-def get_lookahead_point(dense_traj, lookahead_dist):
+def get_lookahead_point(dense_traj, lookahead_dist=1.0):
     acc_dist = 0
     for i in range(dense_traj.shape[0] - 1):
         acc_dist += np.linalg.norm(dense_traj[i + 1, :2] - dense_traj[i, :2])

f110_env_rl.py
@@ -73,9 +73,9 @@ class F110RLEnv(F110Env):
 
         # init params
         self.horizon = int(10)
-        self.predict_time = 1.0  # get waypoints in coming seconds
-        self.rl_max_speed = 5.0
-        self.offset = [0.0] * self.horizon  # self.offset = [0.5] * self.horizon
+        self.predict_time = 2.0  # get waypoints in coming seconds
+        self.rl_max_speed = 2.0
+        self.offset = [0.5] * self.horizon  # self.offset = [0.5] * self.horizon
 
         self.map_max_rows = RaceCar.scan_simulator.map_img.shape[0]
         self.map_max_cols = RaceCar.scan_simulator.map_img.shape[1]
@@ -147,7 +147,7 @@ class F110RLEnv(F110Env):
         self.local_offset_traj = get_offset_traj(self.local_horizon_traj, self.offset)
         self.offset_traj = local_to_global(self.obs, self.local_offset_traj)
         dense_offset_traj = densify_offset_traj(self.horizon_traj)  # [x, y, v]
-        lookahead_point_profile = get_lookahead_point(dense_offset_traj, lookahead_dist=1.5)
+        lookahead_point_profile = get_lookahead_point(dense_offset_traj, lookahead_dist=1.0)
         steering, speed = self.controller.rl_control(self.obs, lookahead_point_profile, max_speed=self.rl_max_speed)
 
         # step function in race car, time step is k+1 now
@@ -174,9 +174,14 @@ class F110RLEnv(F110Env):
         filtered_traj_indices = all_indices[
             (all_indices[:, 1] < self.map_max_rows) & (all_indices[:, 0] < self.map_max_cols)]
 
+        # Standard talk avoid 2 obs config
+        # reward = 100 * step_time
+        # reward -= 0.1 * np.linalg.norm(offset, ord=2)
+        # reward -= 1 * np.linalg.norm((offset[1:] - offset[:-1]), ord=2)
+
         reward = 100 * step_time
-        reward -= 0.1 * np.linalg.norm(offset, ord=2)
-        reward -= 1 * np.linalg.norm((offset[1:] - offset[:-1]), ord=2)
+        reward -= 1 * np.linalg.norm(offset, ord=2)
+        # reward -= 1 * np.linalg.norm((offset[1:] - offset[:-1]), ord=2)
         # reward -= 100 * np.count_nonzero(
         #     RaceCar.scan_simulator.map_img[filtered_traj_indices[:, 1], filtered_traj_indices[:, 0]] == 0)
 

inference.py
@@ -29,7 +29,7 @@ def main():
     rl_planner = True  # enable if you use RL planner
 
     # load map & waypoints
-    map_name = 'skir_blocked'  # levine_2nd, skir, skir_blocked, Spielberg, MoscowRaceway, Catalunya
+    map_name = 'skir'  # levine_2nd, skir, skir_blocked, Spielberg, MoscowRaceway, Catalunya
     map_path = os.path.abspath(os.path.join('maps', map_name))
     csv_data = np.loadtxt(map_path + '/' + map_name + '_raceline.csv', delimiter=';', skiprows=0)  # '_centerline.csv'
     waypoints = WaypointLoader(map_name, csv_data)
@@ -76,7 +76,8 @@ def main():
     rl_env = F110RLEnv(render=False, map_name=map_name, num_obstacles=num_obstacles, obt_poses=obt_pose,
                        num_lidar_scan=108)
     model = Agent(rl_env)
-    model.load_state_dict(torch.load(f'models/skir_blocked_256_pass_2_obs.pkl'))
+    # model.load_state_dict(torch.load(f'models/skir_simpler_input.pkl'))
+    model.load_state_dict(torch.load(f'skir_bootstrap_1m.pkl'))
 
     while not done:
         if method == 'pure_pursuit' and rl_planner:
@@ -102,7 +103,7 @@ def main():
             local_offset_traj = get_offset_traj(local_horizon_traj, offset)
             offset_traj = local_to_global(obs, local_offset_traj)
             dense_offset_traj = densify_offset_traj(offset_traj)  # [x, y, v]
-            lookahead_point_profile = get_lookahead_point(dense_offset_traj, lookahead_dist=1.5)
+            lookahead_point_profile = get_lookahead_point(dense_offset_traj, lookahead_dist=1.0)
             steering, speed = controller.rl_control(obs, lookahead_point_profile, max_speed=rl_env.rl_max_speed)
             speed = 2.0
 

ppo_continuous.py
@@ -51,7 +51,7 @@ def parse_args():
                         help="the learning rate of the optimizer")
     parser.add_argument("--num-envs", type=int, default=2,  # multi-thread envs = 2
                         help="the number of parallel game environments")
-    parser.add_argument("--num-steps", type=int, default=2048,  # cleanrl 2048, for skir >= 5k for finishing 2 laps
+    parser.add_argument("--num-steps", type=int, default=2048,  # cleanrl 2048, for skir >= 5k for finishing 2 laps?
                         help="the number of steps to run in each environment per policy rollout")
     parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
                         help="Toggle learning rate annealing for policy and value networks")
@@ -81,7 +81,7 @@ def parse_args():
     # parameters for rl planner
     parser.add_argument("--render", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
                         help="if toggled, render will be enabled.")
-    parser.add_argument("--map-name", type=str, default="skir_blocked",  # for overfitting
+    parser.add_argument("--map-name", type=str, default="skir",  # for overfitting
                         help="the map of the environment")
     parser.add_argument("--num-obstacles", type=int, default=0,  # for overfitting
                         help="number of randomly generated obstacles")
@@ -211,7 +211,7 @@ if __name__ == "__main__":
     assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
 
     agent = Agent(envs).to(device)
-    # agent.load_state_dict(torch.load('skir_blocked_neighbor_dist_penalty.pkl'))  # 回锅肉！
+    agent.load_state_dict(torch.load('skir_bootstrap_1m.pkl'))  # 回锅肉！
     optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
 
     # ALGO Logic: Storage setup
@@ -390,11 +390,11 @@ if __name__ == "__main__":
                     video_filenames.add(filename)
                     
         if (update % int(num_updates / 5)) == 0:
-            torch.save(agent.state_dict(), Path(f'skir_with_predict_'+str(save_count)+'.pkl'))
+            torch.save(agent.state_dict(), Path(f'skir_bootstrap_2m_'+str(save_count)+'.pkl'))
             print("save model")
             save_count += 1
 
-    model_path = Path(f'skir_with_predict_final.pkl')
+    model_path = Path(f'skir_bootstrap_2m.pkl')
     torch.save(agent.state_dict(), model_path)
 
     envs.close()

utils/traj_utils.py
@@ -8,7 +8,7 @@ from scipy.spatial import distance
 from scipy.interpolate import interp1d
 
 
-def get_front_traj(obs, profile, predict_time=2):
+def get_front_traj(obs, profile, predict_time=2.0):
     waypoints = np.array([profile.x, profile.y]).T
     ref_speed = profile.v
     # ref_curvature = profile.γ
@@ -138,6 +138,7 @@ def local_to_global(obs, local_data):
 
     return global_data
 
+
 def bresenham_line_point(p1, p2):
     points = []
     dx = abs(p2[0] - p1[0])
@@ -146,14 +147,14 @@ def bresenham_line_point(p1, p2):
     sy = 0.1 if p1[1] < p2[1] else -0.1
     # sx = 1 if p1[0] < p2[0] else -1
     # sy = 1 if p1[1] < p2[1] else -1
-    
+
     err = dx + dy
     x0 = p1[0]
     y0 = p1[1]
     while True:
         points.append((x0, y0))
         # if x0 == p2[0] and y0 == p2[1]:
-        if abs(x0 - p2[0])<0.2 and abs(y0 - p2[1])<0.2:
+        if abs(x0 - p2[0]) < 0.2 and abs(y0 - p2[1]) < 0.2:
             break
         e2 = 2 * err
         if e2 >= dy:
@@ -162,7 +163,7 @@ def bresenham_line_point(p1, p2):
         if e2 <= dx:
             err += dx
             y0 += sy
-            
+
     return np.array(points)
 
 
@@ -174,14 +175,14 @@ def bresenham_line_index(p1, p2):
     # sy = 0.1 if p1[1] < p2[1] else -0.1
     sx = 1 if p1[0] < p2[0] else -1
     sy = 1 if p1[1] < p2[1] else -1
-    
+
     err = dx + dy
     x0 = p1[0]
     y0 = p1[1]
     while True:
         points.append((x0, y0))
         if x0 == p2[0] and y0 == p2[1]:
-        # if abs(x0 - p2[0])<0.2 and abs(y0 - p2[1])<0.2:
+            # if abs(x0 - p2[0])<0.2 and abs(y0 - p2[1])<0.2:
             break
         e2 = 2 * err
         if e2 >= dy:
@@ -190,5 +191,5 @@ def bresenham_line_index(p1, p2):
         if e2 <= dx:
             err += dx
             y0 += sy
-            
+
     return np.array(points)

utils/waypoint_loader.py
@@ -15,6 +15,6 @@ class WaypointLoader:
               or map_name == 'levine_2nd' or map_name == 'skir' or map_name == 'skir_blocked'):
             self.x = csv_data[:, 1]
             self.y = csv_data[:, 2]
-            self.v = csv_data[:, 5]
+            self.v = np.full(csv_data[:, 5].shape[0], 2.0)  # csv_data[:, 5]
             self.θ = csv_data[:, 3] + np.pi / 2  # coordinate matters!
             self.γ = csv_data[:, 4]